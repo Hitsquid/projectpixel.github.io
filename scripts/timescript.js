e292777ddbf258e2cae6512de3489ce44a6c510c
ItsSamii <146092360+ItsSamii@users.noreply.github.com>
2023-10-03 14:01:22 +0200

    added content to page

---
+++ scripts/timescript.js
@@ -0,0 +1,33 @@
+const now = new Date();
+const day = now.getDay(); // returns a number representing the day of the week, starting with 0 for Sunday
+const hours = now.getHours();
+const minutes = now.getMinutes();
+console.log(`Today is day ${day} and the time is ${hours}:${minutes}.`);
+
+document.getElementById("time").innerHTML = "Today is day " + day + " and the time is " + hours + ":" + minutes + ".";
+
+
+if (day == 1 || day == 2) {
+    document.getElementById("open").src = "images/closed.png";
+    document.getElementById("open").alt = "closed sign";
+}
+
+else if (hours < 14) {
+    document.getElementById("open").src = "images/closed.png";
+    document.getElementById("open").alt = "closed sign";
+
+}
+else if (day !=0 && hours >= 23) {
+    document.getElementById("open").src = "images/closed.png";
+    document.getElementById("open").alt = "closed sign";
+} 
+
+else if (day == 0 && hours >=22) {
+    document.getElementById("open").src = "images/closed.png";
+    document.getElementById("open").alt = "closed sign";
+}
+
+else {
+    document.getElementById("open").src = "images/open.jpg";
+    document.getElementById("open").alt = "open sign";
+};
