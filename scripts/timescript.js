bdcd56926bbd6f6c7ecf5c4a526e019c80c5fa57
ItsSamii <146092360+ItsSamii@users.noreply.github.com>
2023-10-24 16:20:14 +0200

    updated documents to account for folder changes

--- scripts/timescript.js
+++ scripts/timescript.js
@@ -7,30 +7,30 @@ console.log(`Today is day ${day} and the time is ${hours}:${minutes}.`);
 
 //if else statement to determine if the arcade is open
 if (day == 1 || day == 2) {
-    document.getElementById("sign").src = "images/closed.png";
+    document.getElementById("sign").src = "../images/closed.png";
     document.getElementById("sign").alt = "closed sign";
     document.getElementById("signcaption").innerHTML = "We zijn gesloten.";
 }
 
 else if (hours < 14) {
-    document.getElementById("sign").src = "images/closed.png";
+    document.getElementById("sign").src = "../images/closed.png";
     document.getElementById("sign").alt = "closed sign";
     document.getElementById("signcaption").innerHTML = "We zijn gesloten.";
 }
 else if (day !=0 && hours >= 23) {
-    document.getElementById("sign").src = "images/closed.png";
+    document.getElementById("sign").src = "../images/closed.png";
     document.getElementById("sign").alt = "closed sign";
     document.getElementById("signcaption").innerHTML = "We zijn gesloten.";
 } 
 
 else if (day == 0 && hours >=22) {
-    document.getElementById("sign").src = "images/closed.png";
+    document.getElementById("sign").src = "../images/closed.png";
     document.getElementById("sign").alt = "closed sign";
     document.getElementById("signcaption").innerHTML = "We zijn gesloten.";
 }
 
 else {
-    document.getElementById("sign").src = "images/open.png";
+    document.getElementById("sign").src = "../images/open.png";
     document.getElementById("sign").alt = "open sign";
     document.getElementById("signcaption").innerHTML = "We zijn open.";
 };
