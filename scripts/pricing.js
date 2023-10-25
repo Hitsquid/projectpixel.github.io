b2005b996a533ed70e187b041f5d9200733d7e92
swaggest17 <146092285+swaggest17@users.noreply.github.com>
2023-10-25 11:48:50 +0200

    fixed conflict due to conflicting attention

--- scripts/pricing.js
+++ scripts/pricing.js
@@ -2,8 +2,19 @@
 // Made by Gwyn
 // 04/10/2023
 
+// Yeesh, where to begin? A fucktonne-a code, but luckily most things are repeated.
+// Really, there's only like 5 different things?
+// There's the const weekday, new Date(), the vars for easy collectivisation, then the fun parts; full lists for prices, then the returns per day.
+// I could probably change those days to be shorter, cuz there's 7 days and only 3 states? I think this is nicer, makes it easier for changes in schedules.
+// As for the actual explanations, those'll be above the parts themselves.
+
+// Right, so the date in any coding is seen as a number from 0 through 6, 0 being Sunday, 1 being Monday and so on.
+// Here those numbers get translated to days. Not really needed, but I like this because it requires less thinking.
 const weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
 
+// This block consists of only date things. First, a random letter gets attached to the Date() function.
+// This variable is then used to acsertain other pieces of information, here it's only to get the weekday.
+// After that, we capitalise 
 const d = new Date();
 let day = weekday[d.getDay()];
 var dayNow = day.toUpperCase();
