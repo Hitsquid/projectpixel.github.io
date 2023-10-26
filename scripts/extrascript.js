f83f416fd1b0409d43e5e4fc3bd65be527b3f164
ItsSamii <146092360+ItsSamii@users.noreply.github.com>
2023-10-26 10:29:01 +0200

    added stupid rules

---
+++ scripts/extrascript.js
@@ -0,0 +1,28 @@
+/*
+auteur: Samii, Loek, Gwyn
+datum: 26-10-23
+
+content: script for the extra page to comply with requirements we dont implement on the main pages
+*/
+
+var name = prompt("wat is uw naam?");
+
+if(name.toLowerCase() == "samii" || name.toLowerCase() == "loek" || name.toLowerCase() == "gwyn") 
+{
+    var developer = true;
+}
+
+else
+{
+    developer = false;
+}
+
+if( developer == true)
+{
+    document.getElementById(welcomemessage).innerHTML = "Hallo " + name.toLowerCase() + " jij bent een van de developers van de website";
+}
+
+else
+{
+    
+}
