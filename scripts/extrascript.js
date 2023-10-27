6c50afa0ddb7718df5e72b8e3186d6bf845843f3
ItsSamii <146092360+ItsSamii@users.noreply.github.com>
2023-10-27 13:51:18 +0200

    does this work?

--- scripts/extrascript.js
+++ scripts/extrascript.js
@@ -15,7 +15,7 @@ if(developer == true)
     document.getElementById("welcomemessage").innerHTML = "Hallo " + name.toLowerCase() + ", jij bent een van de developers van de website.";
 }
 
-else if(name === null) 
+else if(name === undefined) 
 {
     document.getElementById("welcomemessage").innerHTML = "Hallo, je hebt geen naam ingevuld.";
 }
